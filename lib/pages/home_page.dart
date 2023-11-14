944fd3ed9bf093f2b097814d94846a7c4ba7725f
USER <ridwanfiki487@gmail.com>
2023-11-14 14:40:48 +0700

    custom component

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,25 +14,46 @@ class HomePage extends StatelessWidget {
               style: TextStyle(color: Colors.white),
             )),
         body: Column(
+          children: [
+            CustomMenu(imageAssets: "assets/images.jpg",title: "segitiga",),
+            CustomMenu(imageAssets: "assets/download.png",title: "jajar genjang",),
+            Container(
+              margin: EdgeInsets.all(8),
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              decoration: BoxDecoration(color: Colors.green),
+              child: Column(
+                children: [
+                  Image.asset("assets/images.jpg"),
+                  Text("Persegi"),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAssets, required this.title,
+  });
+  final String imageAssets;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(color: Colors.green),
+      child: Column(
         children: [
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical:8, horizontal:16),
-            decoration: BoxDecoration(
-                color: Colors.green
-            ),
-            child: Text("Persegi"),
+          Image.asset(
+            imageAssets,
           ),
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical:8, horizontal:16),
-            decoration: BoxDecoration(
-                color: Colors.green
-            ),
-            child: Text("Persegi"),
-          ),
-      ],
-    )
+          Text(title),
+        ],
+      ),
     );
   }
 }
